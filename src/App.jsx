af540ecaf4d1b526b31ec09a4091becf328fd347
dmitryRoar <roarlove1337@gmail.com>
2021-05-08 08:00:16 +0700

    added temp animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import classes from './App.module.scss'
 
 import {useRoutes} from './routes'
 
 export const App = () => {
   const routes = useRoutes(true)
+  const elRef = useRef(null)
+
+
+  useEffect(() => {
+    elRef.current.classList.add(classes['fade-in'])
+  }, [])
 
   return (
-    <main className="container">
+    <main className={`container ${classes['container-app']}`} ref={elRef}>
       {routes}
     </main>    
   )
